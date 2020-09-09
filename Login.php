e581e65ef2ec074fdfe669c0bf30ca860be088a9
quanboy <victorquan24@gmail.com>
2020-09-09 13:26:37 -0400

    updated API that connects successfully

--- Login.php
+++ Login.php
@@ -6,15 +6,17 @@
 	$Username = "";
 	$Password = "";
 
-	$conn = new mysqli("localhost", "cop4311group30", "Cop!'43119'", "cop4311g_contactmanager");
+	$conn = new mysqli("localhost", "cop4311g_30", "Pooppoop2424!", "cop4311g_contactmanager");
 	if ($conn->connect_error)
 	{
+		echo "Error with connection";
 		returnWithError( $conn->connect_error );
 	}
 	else
 	{
-		$sql = "SELECT UserID,Username,Password FROM Users where Username='" . $inData["Username"] . "' and Password='" . $inData["Password"] . "'";
+		$sql = "SELECT Username,Password FROM Users where Username='" . $inData["Username"] . "' and Password='" . $inData["Password"] . "'";
 		$result = $conn->query($sql);
+		echo $sql;
 		if ($result->num_rows > 0)
 		{
 			$row = $result->fetch_assoc();
@@ -45,12 +47,14 @@
 	function returnWithError( $err )
 	{
 		$retValue = '{"UserID":0,"Username":"","Password":"","error":"' . $err . '"}';
+		echo "Returned error";
 		sendResultInfoAsJson( $retValue );
 	}
 
 	function returnWithInfo( $Username, $Password, $UserID )
 	{
 		$retValue = '{"UserID":' . $UserID . ',"Username":"' . $Username . '","Password":"' . $Password . '","error":""}';
+		echo "Returned with info error";
 		sendResultInfoAsJson( $retValue );
 	}
 
