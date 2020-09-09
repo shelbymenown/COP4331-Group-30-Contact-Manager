38551746343e52cff3936799d5c59cddd51c6465
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-09-09 14:21:33 -0400

    Fix HTTP response codes and content

--- Login.php
+++ Login.php
@@ -5,18 +5,21 @@
 	$UserID = 0;
 	$Username = "";
 	$Password = "";
-
+    
 	$conn = new mysqli("localhost", "cop4311g_30", "Pooppoop2424!", "cop4311g_contactmanager");
+	
 	if ($conn->connect_error)
 	{
-		echo "Error with connection";
+		// Server error
+		http_response_code ( 500 );
 		returnWithError( $conn->connect_error );
+	    
 	}
 	else
 	{
 		$sql = "SELECT Username,Password FROM Users where Username='" . $inData["Username"] . "' and Password='" . $inData["Password"] . "'";
 		$result = $conn->query($sql);
-		echo $sql;
+		
 		if ($result->num_rows > 0)
 		{
 			$row = $result->fetch_assoc();
@@ -28,8 +31,12 @@
 		}
 		else
 		{
-			returnWithError( "No Records Found" );
+			// 401 - Unauthorized
+			http_response_code ( 401 );
+			returnWithError( "Username or password don't match" );
 		}
+
+		// Cleanup
 		$conn->close();
 	}
 
@@ -46,15 +53,13 @@
 
 	function returnWithError( $err )
 	{
-		$retValue = '{"UserID":0,"Username":"","Password":"","error":"' . $err . '"}';
-		echo "Returned error";
+		$retValue = '{"error" :"' . $err . '"}';
 		sendResultInfoAsJson( $retValue );
 	}
 
 	function returnWithInfo( $Username, $Password, $UserID )
 	{
 		$retValue = '{"UserID":' . $UserID . ',"Username":"' . $Username . '","Password":"' . $Password . '","error":""}';
-		echo "Returned with info error";
 		sendResultInfoAsJson( $retValue );
 	}
 
