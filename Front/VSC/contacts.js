136f06b44824cf93338f102c5f6c7539caa4ccb4
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-09-20 13:15:48 -0400

    fix collision with bootstrap classes and preapre js functionality

--- Front/VSC/contacts.js
+++ Front/VSC/contacts.js
@@ -2,26 +2,34 @@ var URL_BASE = 'http://spadecontactmanager.com/LAMPAPI';
 var API_EXTENSION = 'php';
 
 var userId = 0;
+var searchQry = ''
+var page = 0;
 var loadedContacts = [];
 var container;
 
+function getInt(val, def=0) {
+	if (!isNaN(val) && parseInt(Number(val) == val) && !isNaN(parseInt(val, 10)))
+		return parseInt(val, 10);
+	else return def;
+}
+
 // Handle page load
 $(document).ready(function () {
+	var urlParams = new URLSearchParams(window.location.search);
 	userId = Cookies.get("userId");
-	// page = queryParams("page")
 
-	// TODO : Change page to /contacts??
+	page = urlParams.has("page") ? getInt(urlParams.get("page")) : 0;
+	searchQry = urlParams.has("search") ? urlParams.get("search") : '';
+
+	// Change Auth page if not logged in
 	if (!userId || userId < 0)
 	{
 		alert("You must log in to view this page!");
 		window.location.pathname = "";
 	}
 
-	// TODO : get page number and get according contacts
-	// TODO : Modify Pagination at the bottom
-
 	// TODO : Load Contacts from API!!!!!
-	loadedContacts = getContacts(userId, page);
+	loadedContacts = getContacts(userId, searchQry, page);
 
 	// TODO : Display Contacts in page
 	if (loadedContacts) {
@@ -53,8 +61,28 @@ function doCreate(e) {
 	// window.location.pathname = "/create.html";
 }
 
-function getContacts(userId, page) {
+function getContacts(userId, searchQry, page) {
+	
+	let uri = `${URL_BASE}/searchcontact${API_EXTENSION ? "." : ""}${API_EXTENSION}`
+	let payload = { userId: userId, searchName: searchQry, page: page };
+
+	let contacts = [];
+
+	$.get(uri, JSON.stringify(payload))
+		.done(function (res){
+			console.log(res);
+			contacts = res;
+		})
+		.fail(function (jqXHR, textStatus, errorThrown) {
+			// TODO : handle error
+			errMsg = jqXHR.responseJSON && jqXHR.responseJSON.error ? jqXHR.responseJSON.error + "😢" : "An error has occured 😟";
+			console.log(jqXHR); console.log(textStatus); console.log(errorThrown);
+
+			// Display error
+			showError($("#signup-error"), errMsg)
+		});
+	
 	// payload = {userId: userId, page: page}
 	alert("getContacts() not functional yet");
-	return [];
+	return contacts;
 }
