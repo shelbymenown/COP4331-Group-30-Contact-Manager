7fed9ae12365579e96789b60bedae17301cbc95d
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-09-21 17:49:21 -0400

    JWT fix and searchContact ready for production

--- searchcontact.php
+++ searchcontact.php
@@ -1,90 +1,107 @@
 <?php
-  //$inData = getRequestInfo();
-  $inData = json_decode(file_get_contents("php://input"), true);
-  $conn = new mysqli("spadecontactmanager.com", "cop4311g_30", "Copcop24!!","cop4311g_contactmanager");
+	// $inData = json_decode(file_get_contents("php://input"), true);
+	$serverKey = '5f2b5cdbe5194f10b3241568fe4e2b24';
+	$ITEMS_PER_PAGE = 5;
+
+	$inData = array();
+	parse_str($_SERVER['QUERY_STRING'], $inData);
+
+	// returnWithError( $_SERVER['HTTP_X_ACCESS_TOKEN'] );
+
+	$searching = isset($inData['search']) ? $inData['search'] : '';
+	$userId = 0;
+	$token = '';
+	$page = 0;
+
+	// Check request body
+	// (1) Token
+	$userId = check_token($_SERVER["HTTP_X_ACCESS_TOKEN"]);
 
 
-    
-    $datas = array();
-  if ($conn->connect_error)
+	// Check request body
+	// (2) Page
+	if (isset($inData["page"]))
 	{
-	    // Server error
-		http_response_code ( 500 );
-		returnWithError( $conn->connect_error );
+		if (!ctype_digit($inData["page"]))
+		{
+			// Bad Request
+			http_response_code ( 400 );
+			returnWithError( "Page must be an integer!" );
+		}
+		$page = (int)$inData["page"];
 	}
-  else
+	
+
+	// Connect to database
+	$conn = new mysqli("spadecontactmanager.com", "cop4311g_30", "Copcop24!!", "cop4311g_contactmanager");
+	if ($conn->connect_error)
 	{
-        //Grabbing the search field, page number needed, and token from user
-        if(isset($inData['searchName'], $inData['page']))//, $inData['token']))
-        {
-            $searching = $inData['searchName'];
-            $pageNumber = (int)$inData['page'];
-            $startingPage = 5 * $pageNumber;
-            //Replaces all characters that arent letters or numbers with space
-            $sql = "SELECT * FROM Contact WHERE (FirstName LIKE '%$searching%' OR LastName LIKE  '%$searching%') LIMIT $startingPage,5 ";
+		// Server error - unable to connect to db
+		http_response_code(500);
+		returnWithError($conn->connect_error);
+	}
+	else // Connected successfully
+	{
+		// $searching
+		// $token
+		// $page
+		$datas = array();
+		$startingPage = $page * $ITEMS_PER_PAGE;
 
-            $result = $conn->query($sql);
-            //echo  $result->num_rows;
-            
-            //If no search was found
-            if($result->num_rows == 0)
-            {
-                //If no entries are found
-                sendResultInfoAsJson('{}');
-            }
-            else
-            {
-                // output data of each row
-                  while($row = mysqli_fetch_assoc($result)) 
-                  {
-                      $datas[] = $row;
-                      
-                  }
-            }
-        }
-        
-        //Determining user page number currently
-        if(!isset($_GET['page']))
-        {
-            //If no page is selected, default to first page
-            $page = 1;
-        }
-        else
-        {
-            //Else, grab whatever page they're on
-            $page = $_GET['page'];
-        }
+		$sql = "SELECT UserID, ContactID as id, FirstName as firstName, LastName as lastName, EmailAddress as email, PhoneNumber as phone FROM Contact
+				WHERE UserId = $userId AND (FirstName LIKE '%$searching%' OR LastName LIKE  '%$searching%')
+				LIMIT $startingPage, $ITEMS_PER_PAGE
+			";
+		$result = $conn->query($sql);
 
+		// echo "123";
 
-        //Creating the pagination
-        $results_per_page = 5;
-        $num_of_results = mysqli_num_rows($result);
+		// output data of each row
+		while ($row = mysqli_fetch_assoc($result))
+		{
+			$datas[] = $row;
+		}
 
-        $num_of_pages = ceil($num_of_results/$results_per_page);
+		// echo "444";
 
-        //Displaying the links to the pages
-        for($page = 1; $page<=$num_of_pages; $page++)
-        {
-            //echo '<a href="https://spadecontactmanager.com/LAMPAPI/randcontact.php?page=' . $page . '">' . $page . '</a>';
-        }
+		// echo "[]";
+		// sendResultInfoAsJson( "{}" );
+		sendResultInfoAsJson( "{" . json_encode($datas) . "}" );
 
-
-
-        echo json_encode($datas);
 		// Cleanup
 		$conn->close();
 	}
 
-    function returnWithError( $err )
+	function returnWithError($err)
 	{
 		$retValue = '{"error" :"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
+		sendResultInfoAsJson($retValue);
+		exit();
 	}
 
-    function sendResultInfoAsJson( $obj )
+	function sendResultInfoAsJson($obj)
 	{
 		header('Content-type: application/json');
 		echo $obj;
 	}
 
+	function IsNullOrEmptyString($str)
+	{
+		return (!isset($str) || trim($str) === '');
+	}
+
+	function check_token($token)
+	{
+		global $serverKey;
+		require_once('jwt.php');
+
+		// Decode token
+		try { return JWT::decode($token, $serverKey, array('HS256'))->userId; }
+		catch(Exception $e)
+		{
+			// Unauthorized
+			http_response_code( 401 );
+			returnWithError( "TOKEN ERROR: {$e->getMessage()}" );
+		}
+	}
 ?>
