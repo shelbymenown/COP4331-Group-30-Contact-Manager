4e59a2e79a54cf57fdac410e661a90ad42daf29d
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-09-21 17:49:21 -0400

    JWT fix and searchContact ready for production

--- Login.php
+++ Login.php
@@ -1,98 +1,99 @@
 <?php
+$serverKey = '5f2b5cdbe5194f10b3241568fe4e2b24';
 
-	$serverKey = '5f2b5cdbe5194f10b3241568fe4e2b24';
+$inData = getRequestInfo();
+
+$UserID = 0;
+$Username = "";
+
+if (IsNullOrEmptyString($inData["Username"]) && IsNullOrEmptyString($inData["Password"]))
+{
+	// Bad Request
+	http_response_code ( 400 );
+	returnWithError( "Username and Password are required for Login!" );
+}
+elseif (IsNullOrEmptyString($inData["Username"]))
+{
+	// Bad Request
+	http_response_code ( 400 );
+	returnWithError( "Username is required for Login!" );
+}
+elseif (IsNullOrEmptyString($inData["Password"]))
+{
+	// Bad Request
+	http_response_code ( 400 );
+	returnWithError( "Password is required for Login!" );
+}
+
+else
+{
+
+	$conn = new mysqli("localhost", "cop4311g_30", "Copcop24!!", "cop4311g_contactmanager");
 	
-	$inData = getRequestInfo();
-
-	$UserID = 0;
-	$Username = "";
-
-	if (IsNullOrEmptyString($inData["Username"]) && IsNullOrEmptyString($inData["Password"]))
+	if ($conn->connect_error)
 	{
-		// Bad Request
-		http_response_code ( 400 );
-		returnWithError( "Username and Password are required for Login!" );
+		// Server error
+		http_response_code ( 500 );
+		returnWithError( $conn->connect_error );
 	}
-	elseif (IsNullOrEmptyString($inData["Username"]))
-	{
-		// Bad Request
-		http_response_code ( 400 );
-		returnWithError( "Username is required for Login!" );
-	}
-	elseif (IsNullOrEmptyString($inData["Password"]))
-	{
-		// Bad Request
-		http_response_code ( 400 );
-		returnWithError( "Password is required for Login!" );
-	}
-
 	else
 	{
-
-		$conn = new mysqli("localhost", "cop4311g_30", "Copcop24!!", "cop4311g_contactmanager");
+		$sql = "SELECT Username, Name, UserID FROM User WHERE Username='" . $inData["Username"] . "' and Password='" . $inData["Password"] . "'";
+		$result = $conn->query($sql);
 		
-		if ($conn->connect_error)
+		if ($result->num_rows > 0)
 		{
-			// Server error
-			http_response_code ( 500 );
-			returnWithError( $conn->connect_error );
+			$row = $result->fetch_assoc();
+			$Username = $row["Username"];
+			$UserID = $row["UserID"];
+			
+			returnWithInfo( $UserID );
 		}
 		else
 		{
-			$sql = "SELECT Username, Name, UserID FROM User WHERE Username='" . $inData["Username"] . "' and Password='" . $inData["Password"] . "'";
-			$result = $conn->query($sql);
-			
-			if ($result->num_rows > 0)
-			{
-				$row = $result->fetch_assoc();
-				$Username = $row["Username"];
-				$UserID = $row["UserID"];
-				
-				returnWithInfo( $UserID, $Username );
-			}
-			else
-			{
-				// 401 - Unauthorized
-				http_response_code ( 401 );
-				returnWithError( "Username or password don't match" );
-			}
-			
-			// Cleanup
-			$conn->close();
+			// 401 - Unauthorized
+			http_response_code ( 401 );
+			returnWithError( "Username or password don't match" );
 		}
-	}
 		
-	function getRequestInfo()
-	{
-		return json_decode(file_get_contents('php://input'), true);
+		// Cleanup
+		$conn->close();
 	}
+}
+	
+function getRequestInfo()
+{
+	return json_decode(file_get_contents('php://input'), true);
+}
 
-	function sendResultInfoAsJson( $obj )
-	{
-		header('Content-type: application/json');
-		echo $obj;
-	}
+function sendResultInfoAsJson( $obj )
+{
+	header('Content-type: application/json');
+	echo $obj;
+}
 
-	function returnWithError( $err )
-	{
-		$retValue = '{"error" :"' . $err . '"}';
-		sendResultInfoAsJson( $retValue );
-	}
+function returnWithError( $err )
+{
+	$retValue = '{"error" :"' . $err . '"}';
+	sendResultInfoAsJson( $retValue );
+}
 
-	function returnWithInfo( $UserID )
-	{
-        require_once('jwt.php');
-        
-        // Encode UserID into a token
-		$payloadArray = array();
-		$payloadArray['userId'] = $UserID;
-		$token = JWT::encode($payloadArray, $serverKey);
+function returnWithInfo( $UserID )
+{
+	global $serverKey;
+	
+	require_once('jwt.php');
 
-        // Respond with token
-        sendResultInfoAsJson( '{"token": "' . $token . '"}' );
-	}
+	// Encode UserID into a token
+	$payloadArray = array();
+	$payloadArray['userId'] = $UserID;
+	$token = JWT::encode($payloadArray, $serverKey);
 
-	function IsNullOrEmptyString($str){
-		return (!isset($str) || trim($str) === '');
-	}
+	// Respond with token
+	sendResultInfoAsJson( '{"token": "' . $token . '"}' );
+}
+
+function IsNullOrEmptyString($str){
+	return (!isset($str) || trim($str) === '');
+}
 ?>
