1e0bc0a90bb81341076ca2d5044bff5052e9179a
Aadil <aadilhabibi99@gmail.com>
2020-09-30 19:21:53 -0400

    Updating addcontact

--- addcontact.php
+++ addcontact.php
@@ -1,18 +1,25 @@
 <?php
   $conn = new mysqli("spadecontactmanager.com", "cop4311g_30", "Copcop24!!","cop4311g_contactmanager");
   
+  $serverKey = '5f2b5cdbe5194f10b3241568fe4e2b24';
+
+  $inData = getRequestInfo();
+
+  parse_str($_SERVER['QUERY_STRING'], $inData);
+
+  $userId = check_token($_SERVER["HTTP_X_ACCESS_TOKEN"]);
+
   if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
 	}
 	else
-	{
-		$inData = json_decode(file_get_contents("php://input"));
-		
-	    $sql = "INSERT INTO Contact(UserID, FirstName, LastName, EmailAddress, PhoneNumber) 
-				VALUES('" . $inData->userID . "','" . $inData->FirstName . "', '" . $inData->LastName . "', '" . $inData->Email . "', '" . $inData->Phone . "')";
+	{	
+	    $sql = "INSERT INTO Contact(UserID, FirstName, LastName, EmailAddress, PhoneNumber)
+				VALUES('" . $userId . "','" . $inData->FirstName . "', '" . $inData->LastName . "', '" . $inData->Email . "', '" . $inData->Phone . "')";
 		$result = $conn->query($sql);
-		
+
+
 		if ($result != TRUE)
 		{
 			http_response_code ( 400 );
@@ -20,15 +27,22 @@
 		}
 		else
 		{
+			//$id = mysqli_insert_id($conn);
+			$id = 7;
 			// 201 - Created
 			http_response_code ( 201 );
-			sendResultInfoAsJson('{}');
+			sendResultAsJson('{"id":' . $id . '}');
 		}
 
 		// Cleanup
 		$conn->close();
 	}
 	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
 	function returnWithError( $err )
 	{
 		$retValue = '{"error" :"' . $err . '"}';
