6ed83cd502996af5fc2c0f9f229a84f7948fbef8
Aadil <aadilhabibi99@gmail.com>
2020-10-01 20:13:12 -0400

    Updating updateContact

--- updateContact.php
+++ updateContact.php
@@ -1,6 +1,14 @@
 <?php
 	$conn = new mysqli("spadecontactmanager.com", "cop4311g_30", "Copcop24!!","cop4311g_contactmanager");
   
+	$serverKey = '5f2b5cdbe5194f10b3241568fe4e2b24';
+
+	$inData = getRequestInfo();
+
+	parse_str($_SERVER['QUERY_STRING'], $inData);
+
+	$userId = check_token($_SERVER["HTTP_X_ACCESS_TOKEN"]);
+
 	if ($conn->connect_error)
 	{
 		returnWithError( $conn->connect_error );
@@ -36,6 +44,26 @@
 		$conn->close();
 	}
 	
+	function getRequestInfo()
+	{
+		return json_decode(file_get_contents('php://input'), true);
+	}
+
+	function check_token($token)
+	{
+		global $serverKey;
+		require_once('jwt.php');
+
+		// Decode token
+		try { return JWT::decode($token, $serverKey, array('HS256'))->userId; }
+		catch(Exception $e)
+		{
+			// Unauthorized
+			http_response_code( 401 );
+			returnWithError( "TOKEN ERROR: {$e->getMessage()}" );
+		}
+	}
+
 	function sendResultInfoAsJson( $obj )
 	{
 		header('Content-type: application/json');
