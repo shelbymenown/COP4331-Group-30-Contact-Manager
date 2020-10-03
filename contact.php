116b631be26db48298ac7b0b831e623e2f97e2e5
Gonen Matias <gonen.matias@knights.ucf.edu>
2020-10-03 17:42:22 -0400

    Structure for contact(s) related endpoints

---
+++ contact.php
@@ -0,0 +1,16 @@
+<!-- /contact.php endpoint -->
+
+<!-- TODO -->
+<?php
+	// User actions on a SET of contacts
+	// All methods require a JWT token (userid)
+	// 
+	// GET - search a contact accodring to a query and page number
+	// 
+	// 
+	// 
+	// ANY OTHER METHOD - "Method Not Supported"
+	// 
+?>
+
+
---
+++ contacts.php
@@ -0,0 +1,20 @@
+<!-- /contacts.php endpoint -->
+
+<!-- TODO -->
+<?php
+	// User actions on a particular contact
+	// All methods require a JWT token (userid)
+	// 
+	// GET - Method not supported
+	// 
+	// POST - Create contact
+	// 
+	// PUT - Update contact
+	// 
+	// DELETE - Delete contact
+	// 
+	// 
+	// 
+	// ANY OTHER METHOD - "Method Not Supported"
+	// 
+?>
